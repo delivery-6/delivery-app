f1308bce02db297b2c6f67ebe80d8caedc18058f
mayfly <dngurrb1004@naver.com>
2025-01-09 19:35:22 +0900

    temp

--- src/main/java/com/example/menu/repository/MenuRepository.java
+++ src/main/java/com/example/menu/repository/MenuRepository.java
@@ -8,8 +8,6 @@ import com.querydsl.jpa.JPQLQueryFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 import static com.example.menu.entity.QMenu.menu;
 import static com.example.shop.entity.QShop.shop;
 

27401b093021af021bec99b1701804a2990a5a19
asdd2557 <asdd2557@naver.com>
2025-01-09 20:42:09 +0900

    fix: 필요없는 부분 지우고 exception만 올림

--- src/main/java/com/example/menu/dto/request/MenuCreateRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.menu.dto.request;
-
-public record MenuCreateRequestDto(
-        String name,
-        int price
-) {
-}
--- src/main/java/com/example/user/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.entity;
-
-public enum Role {
-    CUSTOMER, OWNER;
-
-    public String getAuthority() {
-        return "ROLE_" + this.name();
-    }
-}
